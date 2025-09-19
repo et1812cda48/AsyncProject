e391084e486ca75f3b2d1044e598f363cace23e0
et1812cda48 <72542297+et1812cda48@users.noreply.github.com>
2025-09-19 17:42:02 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var rand = new Random();
+            int n_rockets;
+
+            while(true)
+            {
+                Console.Write("Enter rockets' number: ");
+                n_rockets = Convert.ToInt32(Console.ReadLine());
+                if (n_rockets <= 0) break;
+                Console.Clear();
+                Console.WriteLine($"Rockets' number: {n_rockets}");
+                await Run(n_rockets);                
+            }
+        }
+        static async Task Run(int n_rockets)
+        {
+            Rocket rocket;
+            Task[] tasks = new Task[n_rockets];
+            for (var i = 0; i < n_rockets; i++)
+            {
+                rocket = new Rocket($"Rocket №{i + 1}");
+                tasks[i] = rocket.Start();
+            }
+            Task.WaitAll(tasks);
+            Console.WriteLine("\nAll rockets were started!!!\n");
+        }
+    }
+}
---
+++ Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncProject
+{
+    class Rocket
+    {
+        string name;
+        int count;
+        public Rocket(string name)
+        {
+            int max = 28, min = 5;
+            var rand = new Random();
+            this.count = rand.Next(min, max);
+            this.name = name;
+            //this.Start();
+        }
+
+        public async Task Start()
+        {
+            while(this.count > 0)
+            {
+                Console.WriteLine($"{this.name}:\t{this.count}");
+                this.count -= 1;
+                await Task.Delay(1000);
+            }
+            Console.WriteLine($"{this.name}:\trun!!!");
+        }
+    }
+}
